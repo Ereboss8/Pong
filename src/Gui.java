290c4a4ede63336a4cffc64a569850c0dc10a187
Eduard <daneduardleuska@gmail.com>
2025-10-23 15:52:40 +0200

    first gui elements

--- src/Gui.java
+++ src/Gui.java
@@ -1,2 +1,15 @@
-public class Gui {
+import javax.swing.JFrame;
+
+public class Gui extends JFrame {
+    JButton button = new JButton("Click Me");
+    JTextField textField = new JTextField("This is a text field");
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("My GUI Application");
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
